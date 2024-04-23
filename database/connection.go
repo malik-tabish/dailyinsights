8cc6823886d0de31e9fc43d93ae1be23dfa9bc67
Tabish <maliktabish0909@gmail.com>
2024-04-23 16:39:26 +0530

    Database Connection

--- database/connection.go
+++ database/connection.go
@@ -1,1 +1,33 @@
-package database
+package connection
+
+import (
+	"fmt"
+	"log"
+
+	"project/config"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	var err error
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", config.Environment.DBUsername, config.Environment.DBPassword, config.Environment.DBHost, config.Environment.DBPort, config.Environment.DBName)
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error while connecting to database:	%v", err)
+	}
+	log.Println("Database Connected Successfully")
+}
+
+func CloseDB() {
+	mysqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatalf("Error getting DB: %v", err)
+	}
+	if err := mysqlDB.Close(); err != nil {
+		log.Fatalf("Error closing database: %v", err)
+	}
+}
--- models/models.go
+++ models/models.go
@@ -1,1 +1,21 @@
 package models
+
+import (
+	connection "project/database"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"unique;not null"`
+	Email    string `gorm:"unique;not null"`
+	FullName string
+	Password string `gorm:"not null"`
+}
+
+func AutoMigrate() {
+	if err := connection.DB.AutoMigrate(&User{}); err != nil {
+		panic(err)
+	}
+}
